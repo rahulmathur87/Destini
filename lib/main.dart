a26a2715deb02d21e6a617efa6b08819d021a42e
Rahul <rahulmathur87@gmail.com>
2025-12-19 16:43:19 +0530

    1st story and choices visible. completed till TODO15

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
 
-//TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
-
 void main() {
   runApp(Destini());
 }
@@ -64,8 +62,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.red),
                   child: Text(
-                    //TODO: Step 13 - Use the storyBrain to get the text for choice 1.
-                    'Choice 1',
+                    storyBrain.getChoice1(),
                     style: TextStyle(fontSize: 20.0),
                   ),
                 ),
@@ -82,8 +79,7 @@ class _StoryPageState extends State<StoryPage> {
                   },
                   style: TextButton.styleFrom(backgroundColor: Colors.blue),
                   child: Text(
-                    //TODO: Step 14 - Use the storyBrain to get the text for choice 2.
-                    'Choice 2',
+                    storyBrain.getChoice2(),
                     style: TextStyle(fontSize: 20.0),
                   ),
                 ),
